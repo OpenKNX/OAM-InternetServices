998e9cfaca6755f10a74b4885f4b47aaefe0bd15
Michael Geramb <lasso_riefen0m@icloud.com>
2024-05-20 22:03:57 +0200

    Test Version

---
+++ include/InternetServicesESP32-Test.h
@@ -0,0 +1,178 @@
+#pragma once
+
+
+#define paramDelay(time) (uint32_t)( \
+            (time & 0xC000) == 0xC000 ? (time & 0x3FFF) * 100 : \
+            (time & 0xC000) == 0x0000 ? (time & 0x3FFF) * 1000 : \
+            (time & 0xC000) == 0x4000 ? (time & 0x3FFF) * 60000 : \
+            (time & 0xC000) == 0x8000 ? ((time & 0x3FFF) > 1000 ? 3600000 : \
+                                         (time & 0x3FFF) * 3600000 ) : 0 )
+                                             
+#define MAIN_OpenKnxId 0xAE
+#define MAIN_ApplicationNumber 47
+#define MAIN_ApplicationVersion 13
+#define MAIN_ParameterSize 46
+#define MAIN_MaxKoNumber 0
+#define MAIN_OrderNumber "MGKnxINET"
+#define BASE_ModuleVersion 17
+#define SHC_ModuleVersion 0
+// Parameter with single occurrence
+
+
+#define BASE_StartupDelayBase                     0      // 2 Bits, Bit 7-6
+#define     BASE_StartupDelayBaseMask 0xC0
+#define     BASE_StartupDelayBaseShift 6
+#define BASE_StartupDelayTime                     0      // 14 Bits, Bit 13-0
+#define     BASE_StartupDelayTimeMask 0x3FFF
+#define     BASE_StartupDelayTimeShift 0
+#define BASE_HeartbeatDelayBase                   2      // 2 Bits, Bit 7-6
+#define     BASE_HeartbeatDelayBaseMask 0xC0
+#define     BASE_HeartbeatDelayBaseShift 6
+#define BASE_HeartbeatDelayTime                   2      // 14 Bits, Bit 13-0
+#define     BASE_HeartbeatDelayTimeMask 0x3FFF
+#define     BASE_HeartbeatDelayTimeShift 0
+#define BASE_Timezone                             4      // 5 Bits, Bit 7-3
+#define     BASE_TimezoneMask 0xF8
+#define     BASE_TimezoneShift 3
+#define BASE_TimezoneSign                         4      // 1 Bit, Bit 7
+#define     BASE_TimezoneSignMask 0x80
+#define     BASE_TimezoneSignShift 7
+#define BASE_TimezoneValue                        4      // 4 Bits, Bit 6-3
+#define     BASE_TimezoneValueMask 0x78
+#define     BASE_TimezoneValueShift 3
+#define BASE_CombinedTimeDate                     4      // 1 Bit, Bit 2
+#define     BASE_CombinedTimeDateMask 0x04
+#define     BASE_CombinedTimeDateShift 2
+#define BASE_SummertimeAll                        4      // 2 Bits, Bit 1-0
+#define     BASE_SummertimeAllMask 0x03
+#define     BASE_SummertimeAllShift 0
+#define BASE_SummertimeDE                         4      // 2 Bits, Bit 1-0
+#define     BASE_SummertimeDEMask 0x03
+#define     BASE_SummertimeDEShift 0
+#define BASE_SummertimeWorld                      4      // 2 Bits, Bit 1-0
+#define     BASE_SummertimeWorldMask 0x03
+#define     BASE_SummertimeWorldShift 0
+#define BASE_SummertimeKO                         4      // 2 Bits, Bit 1-0
+#define     BASE_SummertimeKOMask 0x03
+#define     BASE_SummertimeKOShift 0
+#define BASE_Latitude                             5      // float
+#define BASE_Longitude                            9      // float
+#define BASE_Diagnose                            14      // 1 Bit, Bit 7
+#define     BASE_DiagnoseMask 0x80
+#define     BASE_DiagnoseShift 7
+#define BASE_Watchdog                            14      // 1 Bit, Bit 6
+#define     BASE_WatchdogMask 0x40
+#define     BASE_WatchdogShift 6
+#define BASE_ReadTimeDate                        14      // 1 Bit, Bit 5
+#define     BASE_ReadTimeDateMask 0x20
+#define     BASE_ReadTimeDateShift 5
+#define BASE_HeartbeatExtended                   14      // 1 Bit, Bit 4
+#define     BASE_HeartbeatExtendedMask 0x10
+#define     BASE_HeartbeatExtendedShift 4
+#define BASE_ManualSave                          14      // 3 Bits, Bit 2-0
+#define     BASE_ManualSaveMask 0x07
+#define     BASE_ManualSaveShift 0
+#define BASE_PeriodicSave                        15      // 8 Bits, Bit 7-0
+#define BASE_Dummy                               45      // uint8_t
+
+// Zeitbasis
+#define ParamBASE_StartupDelayBase                    ((knx.paramByte(BASE_StartupDelayBase) & BASE_StartupDelayBaseMask) >> BASE_StartupDelayBaseShift)
+// Zeit
+#define ParamBASE_StartupDelayTime                    (knx.paramWord(BASE_StartupDelayTime) & BASE_StartupDelayTimeMask)
+// Zeit (in Millisekunden)
+#define ParamBASE_StartupDelayTimeMS                  (paramDelay(knx.paramWord(BASE_StartupDelayTime)))
+// Zeitbasis
+#define ParamBASE_HeartbeatDelayBase                  ((knx.paramByte(BASE_HeartbeatDelayBase) & BASE_HeartbeatDelayBaseMask) >> BASE_HeartbeatDelayBaseShift)
+// Zeit
+#define ParamBASE_HeartbeatDelayTime                  (knx.paramWord(BASE_HeartbeatDelayTime) & BASE_HeartbeatDelayTimeMask)
+// Zeit (in Millisekunden)
+#define ParamBASE_HeartbeatDelayTimeMS                (paramDelay(knx.paramWord(BASE_HeartbeatDelayTime)))
+// Zeitzone
+#define ParamBASE_Timezone                            ((knx.paramByte(BASE_Timezone) & BASE_TimezoneMask) >> BASE_TimezoneShift)
+// Zeitzone-Vorzeichen
+#define ParamBASE_TimezoneSign                        ((bool)(knx.paramByte(BASE_TimezoneSign) & BASE_TimezoneSignMask))
+// Zeitzone-Wert
+#define ParamBASE_TimezoneValue                       ((knx.paramByte(BASE_TimezoneValue) & BASE_TimezoneValueMask) >> BASE_TimezoneValueShift)
+// Empfangen über
+#define ParamBASE_CombinedTimeDate                    ((bool)(knx.paramByte(BASE_CombinedTimeDate) & BASE_CombinedTimeDateMask))
+// Sommerzeit ermitteln durch
+#define ParamBASE_SummertimeAll                       (knx.paramByte(BASE_SummertimeAll) & BASE_SummertimeAllMask)
+// Sommerzeit ermitteln durch
+#define ParamBASE_SummertimeDE                        (knx.paramByte(BASE_SummertimeDE) & BASE_SummertimeDEMask)
+// Sommerzeit ermitteln durch
+#define ParamBASE_SummertimeWorld                     (knx.paramByte(BASE_SummertimeWorld) & BASE_SummertimeWorldMask)
+// Sommerzeit ermitteln durch
+#define ParamBASE_SummertimeKO                        (knx.paramByte(BASE_SummertimeKO) & BASE_SummertimeKOMask)
+// Breitengrad
+#define ParamBASE_Latitude                            (knx.paramFloat(BASE_Latitude, Float_Enc_IEEE754Single))
+// Längengrad
+#define ParamBASE_Longitude                           (knx.paramFloat(BASE_Longitude, Float_Enc_IEEE754Single))
+// Diagnoseobjekt anzeigen
+#define ParamBASE_Diagnose                            ((bool)(knx.paramByte(BASE_Diagnose) & BASE_DiagnoseMask))
+// Watchdog aktivieren
+#define ParamBASE_Watchdog                            ((bool)(knx.paramByte(BASE_Watchdog) & BASE_WatchdogMask))
+// Bei Neustart vom Bus lesen
+#define ParamBASE_ReadTimeDate                        ((bool)(knx.paramByte(BASE_ReadTimeDate) & BASE_ReadTimeDateMask))
+// Erweitertes "In Betrieb"
+#define ParamBASE_HeartbeatExtended                   ((bool)(knx.paramByte(BASE_HeartbeatExtended) & BASE_HeartbeatExtendedMask))
+// Manuelles speichern
+#define ParamBASE_ManualSave                          (knx.paramByte(BASE_ManualSave) & BASE_ManualSaveMask)
+// Zyklisches speichern
+#define ParamBASE_PeriodicSave                        (knx.paramByte(BASE_PeriodicSave))
+// 
+#define ParamBASE_Dummy                               (knx.paramByte(BASE_Dummy))
+
+#define BASE_KoHeartbeat 1
+#define BASE_KoTime 2
+#define BASE_KoDate 3
+#define BASE_KoDiagnose 7
+#define BASE_KoIsSummertime 10
+#define BASE_KoManualSave 11
+
+// In Betrieb
+#define KoBASE_Heartbeat                           (knx.getGroupObject(BASE_KoHeartbeat))
+// Uhrzeit/Datum
+#define KoBASE_Time                                (knx.getGroupObject(BASE_KoTime))
+// Datum
+#define KoBASE_Date                                (knx.getGroupObject(BASE_KoDate))
+// Diagnose
+#define KoBASE_Diagnose                            (knx.getGroupObject(BASE_KoDiagnose))
+// Sommerzeit aktiv
+#define KoBASE_IsSummertime                        (knx.getGroupObject(BASE_KoIsSummertime))
+// Speichern
+#define KoBASE_ManualSave                          (knx.getGroupObject(BASE_KoManualSave))
+
+// Communication objects with single occurrence
+#define KoBASE_Heartbeat 1
+#define KoBASE_Time 2
+#define KoBASE_Date 3
+#define KoBASE_Diagnose 7
+#define KoBASE_IsSummertime 10
+#define KoBASE_ManualSave 11
+
+// In Betrieb
+#define KoBASE_Heartbeat                      (knx.getGroupObject(KoBASE_Heartbeat))
+// Uhrzeit/Datum
+#define KoBASE_Time                           (knx.getGroupObject(KoBASE_Time))
+// Datum
+#define KoBASE_Date                           (knx.getGroupObject(KoBASE_Date))
+// Diagnose
+#define KoBASE_Diagnose                       (knx.getGroupObject(KoBASE_Diagnose))
+// Sommerzeit aktiv
+#define KoBASE_IsSummertime                   (knx.getGroupObject(KoBASE_IsSummertime))
+// Speichern
+#define KoBASE_ManualSave                     (knx.getGroupObject(KoBASE_ManualSave))
+
+
+
+// Header generation for Module 'BASE_KommentarModule'
+
+#define BASE_KommentarModuleCount 0
+#define BASE_KommentarModuleModuleParamSize 0
+#define BASE_KommentarModuleSubmodulesParamSize 0
+#define BASE_KommentarModuleParamSize 0
+#define BASE_KommentarModuleParamOffset 46
+#define BASE_KommentarModuleCalcIndex(index, m1) (index + BASE_KommentarModuleParamOffset + _channelIndex * BASE_KommentarModuleCount * BASE_KommentarModuleParamSize + m1 * BASE_KommentarModuleParamSize)
+
+
+
